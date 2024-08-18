efb0395c2ea9c2ccb18b245fc7794b4a97ed6632
Kent 'picat' Gruber <kent.picat.gruber@gmail.com>
2024-08-18 10:30:58 -0400

    Refactor analysis around SSA, remove suspect checks

--- rsacheck/rsacheck_test.go
+++ rsacheck/rsacheck_test.go
@@ -6,16 +6,6 @@ import (
 	"golang.org/x/tools/go/analysis/analysistest"
 )
 
-func TestAnalyzer(t *testing.T) {
-	if Analyzer.Name != "rsalint" {
-		t.Fatalf("Unexpected analyzer name found, exp: %q, got: %q", "rsalint", Analyzer.Name)
-	}
-
-	if Analyzer.Doc == "" {
-		t.Error("No analyzer doc string found.")
-	}
-}
-
 func TestVulnerable(t *testing.T) {
 	analysistest.Run(t, analysistest.TestData(), Analyzer, "vulnerable")
 }
